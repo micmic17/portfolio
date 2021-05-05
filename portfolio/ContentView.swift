26aacfe8c8b09b97793abc174f21f3ef2e4b8051
micmic17 <mickale_saturre@ligph.com>
2021-05-05 10:16:54 +0800

    Initial Commit

---
+++ portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  portfolio
+//
+//  Created by Mickale Saturre on 5/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by Mickale Saturre on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
